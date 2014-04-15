1bc043f4dcfadcf252c4d444a65ae3bde832934d
Raghu Betina <git@rugb.com>
2014-04-14 19:30:04 -0500

    Added ruby basics

--- names.rb
+++ names.rb
@@ -1,1 +1,43 @@
 # This file will hold our script's source code.
+
+def full_name_for(first_name, last_name)
+  # first = "raghu"
+  # last = "betina"
+
+  full = "- " + first_name.capitalize + " " + last_name.capitalize
+
+  puts full
+end
+
+
+full_name_for "Raghu", "Betina"
+full_name_for("Roneesh", "Vashist")
+full_name_for("Ben", "Beyerlein")
+full_name_for("Mark", "Laurenz")
+
+number = 2
+
+if number < 5
+  puts "That's very small!"
+elsif number < 10
+  puts "That's kinda big"
+else
+  puts "That's pretty big"
+end
+
+
+# first = "Ben"
+# last = "Beyerlein"
+
+# full = "- " + first.capitalize + " " + last.capitalize
+
+# puts full
+
+# first = "Mark"
+# last = "Laurenz"
+
+# full = first.capitalize + " " + last.capitalize
+
+# puts full
+
+
